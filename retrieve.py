136a3d08394a926fb1600cb6f99bd68366372f67
Alexander Krasnov <sashakrasnov.nfo@gmail.com>
2021-09-01 23:15:07 +0300

    Update retrieve.py

--- retrieve.py
+++ retrieve.py
@@ -1,3 +1,8 @@
+'''
+UN Comtrade data retriever
+'''
+
+
 import pandas as pd, os
 from urllib.request import urlretrieve
 from time import sleep
@@ -6,23 +11,27 @@ import requests
 import certifi
 import urllib3
 
+
+# List of countries-reporters: id/country
 #reporter_url = 'https://comtrade.un.org/data/cache/reporterAreas.json'
 reporter_url = 'reporterAreas.json'
+
+# List of countries-partners: id/country
 #partner_url = 'https://comtrade.un.org/data/cache/partnerAreas.json'
 partner_url = 'partnerAreas.json'
 
-# период, за который будем вытягивать данные
+# Data interval
 last_year = date.today().year - 1
 years = range(last_year, 2011, -1)
 
-# список стран, для которых будем делать выгрузки. если список пустой, то для всех
+# List of countries to be load. Empty list for all countries
 valid_reporters = ()
 valid_partners  = ()
 
-# в каком виде будем забирать: json или csv. первый вариант более полный
+# Default data type. JSON data from UN Comtrade is more complete than CSV
 data_type = 'json'
 
-# получаем список кодов регионов, они содержатся в колонке 'results'
+# List of regions
 reporters = pd.read_json(reporter_url)
 partners  = pd.read_json(partner_url)
 
@@ -37,31 +46,32 @@ for year in years:
         pass
 
     for reporter in reporters['results']:
-        # исключаем "все страны" и проверяем есть ли что-то в списке стран
+        # excluding unnecessary countries
         if reporter['id'] == 'all' or (valid_reporters and int(reporter['id']) not in valid_reporters):
             continue
 
         for partner in partners['results']:
             fl = dir + '/' + str(reporter['id']) + '-' + str(partner['id']) + '.'
 
-            # Если регион = все, или два региона одинаковы или файл уже есть, переходим к следующему
+            # Skip if all partners, same regions or file already retrieved
             if partner['id'] == 'all' or reporter['id'] == partner['id'] or (valid_partners and int(partner['id']) not in valid_partners):
                 continue
 
             print('Fetching data for', reporter['text'], '-', partner['text'])
 
-            #если уже вытягивали ранее, в независимости от формата
+            # Data already retrieved and saved previously
             if os.path.isfile(fl + 'csv') or os.path.isfile(fl + 'json'):
                 print(fl + '[csv|json]', '-> File already exists\n')
+
                 continue
 
-            # вычисляем адрес нужной выгрузки
+            # URL of UN Comtrade data to be retrieved
             #url = 'https://comtrade.un.org/api/get?max=100000&type=C&freq=A&px=HS&ps=' + str(year) + '&r=' + reporter['id'] + '&p=' + partner['id'] + '&rg=all&cc=ALL&fmt=' + data_type
             url = 'https://comtrade.un.org/api/get?max=100000&type=C&freq=A&px=HS&ps={}&r={}&p={}&rg=all&cc=ALL&fmt={}'.format(year, reporter['id'], partner['id'], data_type)
 
             print(fl + data_type)
 
-            # вариант 1
+            # Var. 1
             http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
 
             try:
@@ -72,7 +82,7 @@ for year in years:
                 with open(fl + data_type, 'wb') as f:
                     f.write(r.data)
             
-                # вариант 2
+                # Var. 2
                 #with open(fl + data_type, 'wb') as f:
                 #    resp = requests.get(url, verify=False)
                 #    f.write(resp.content)
@@ -82,5 +92,5 @@ for year in years:
             except:
                 print('- Error retreiving\n')
 
-            # ждем 40 секунд в соответствии с ограничением API
+            # Wait for 40 sec to skip UN Comtrade API restriction
             sleep(40)
